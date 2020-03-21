f99912a925c37046448470919f249d3855fb0fe0
ofu997 <ofu997@gmail.com>
2020-03-20 21:46:18 -0700

    add search-by-nation form that returns cases

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import axios from 'axios';
-import { requestURL , summary } from './constants';
+import { requestURL , summary, listData } from './constants';
 
 class App extends React.Component {
   constructor(props) {
@@ -14,12 +13,16 @@ class App extends React.Component {
       deaths: "",
       recovered: "",
       error: null,
+      lookup: null,
+      submitted: false,
+      countryArray: [],
     };
-
-    this.loadData = this.loadData.bind(this);
+    this.LoadData = this.LoadData.bind(this);
+    this.Submit = this.Submit.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
-  loadData = () => {
+  LoadData = () => {
     this.setState({loading: true});
     return axios  
       .get(`${requestURL}${summary}`)
@@ -40,19 +43,109 @@ class App extends React.Component {
       });
   };  
 
+  onChange(event) {
+    this.setState({
+      lookup : event.target.value,
+      submitted: false,
+    })
+  }
+
+  Submit = (event) => {
+    this.setState({ 
+      lookup : this.state.lookup,
+      submitted: true,
+      // countryArray : countryArray.push(this.state.lookup),
+    });
+    // this.setState({ lookup : "Canada" });
+    console.log(this.state.lookup);
+    event.preventDefault(); 
+  }  
+
   componentDidMount() {
-    this.loadData();
+    this.LoadData();
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>cases: {this.state.cases} deaths: {this.state.deaths} recoveries: {this.state.recovered}</p>
+          <p><strong>Worldwide</strong></p>
+          <p>cases: { this.state.cases }</p>
+          <p> deaths: { this.state.deaths }</p>
+          <p>recoveries: { this.state.recovered }</p>
         </header>
+        <div className="searchDiv">
+          <Search 
+          onSubmit = { this.Submit } 
+          test="test"
+          value={ this.state.value }
+          onChange = { this.onChange }
+          />
+        </div>
+        <div className="displayResult">
+          {
+            this.state.submitted &&
+            <Result 
+              name = { this.state.lookup }
+              countryArray = { this.state.countryArray }
+            />
+          }
+        </div>
       </div>
     );
   }
 }
+
+const Search = (props) => 
+  <form onSubmit = { props.onSubmit }>
+    <input 
+      type = "text" 
+      placeholder = "Search..." 
+      value={ props.value }
+      onChange = { props.onChange }
+    /> 
+    <button type="submit">
+      Submit
+    </button>
+  </form>
+
+class Result extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      country: null,
+      cases: null,
+      todayCases: null,
+      deaths: null,
+      todayDeaths: null,
+      recovered: null,
+      active: null,
+      critical: null,
+      casesPerOneMillion: null, 
+      // countryArray: [],
+    };
+    this.LoadListData = this.LoadListData.bind(this);
+  }
+  LoadListData = () => {
+    return axios
+      .get(`${listData}` + '/' + `${this.props.name}`)
+      .then(result => {
+        this.setState({
+          cases: result.data.cases,
+        })
+      })
+  }
+  componentDidMount() {
+    this.LoadListData();
+  }
+  render() {
+    return(
+      <div>
+          <p>{this.props.name} cases: {this.state.cases}</p>
+      </div>
+    );
+  };
+}
+
 
 export default App;
--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,10 @@
 const requestURL = 'https://corona.lmao.ninja';
 const summary = '/all';
+const listData = 'https://corona.lmao.ninja/countries'
 
 const setStateOfResponseFromAxios=
 'https://stackoverflow.com/questions/41194866/how-to-set-state-of-response-from-axios-in-react/41196643#41196643';
 
 
 
-export { requestURL , summary }; 
+export { requestURL , summary, listData,}; 
